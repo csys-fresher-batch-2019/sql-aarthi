359a263d06d3dbd4e33c9e2b712fac93c5d0d8e6
Aarthi G <aarthiaruna12111999@gmail.com>
2019-12-24 17:55:52 +0530

    Update student.sql

--- student.sql
+++ student.sql
@@ -1,4 +1,4 @@
 create table student(stud_id number,stud_name varchar2(20));
 
 insert into student values(101,'Aruna');
-                                                       
+select * from student;                                                      
