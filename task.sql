2eee32ebb57a118e67617526c6c5959a9ae20cb2
Aarthi G <aarthiaruna12111999@gmail.com>
2019-12-26 10:38:01 +0530

    Update task.sql

--- task.sql
+++ task.sql
@@ -1,1 +1,4 @@
 create table tasks(task_no number,task_name varchar2(40),name varchar2(25),deadline date,completed_on date,status char(1));
+insert into tasks(task_no,task_name,name,deadline,completed_on,status)values(1,'install oracle','aarthi',to_date('27-12-2019','dd-mm-yyyy'),to_date('26-12-2019',dd-mm-yyyy'),'yes');
+insert into tasks(task_no,task_name,name,deadline,completed_on,status)values(2,'install jdk','revathi',to_date('30-12-2019','dd-mm-yyyy'),to_date('29-12-2019',dd-mm-yyyy'),'no');  
+                                                                                                                       
