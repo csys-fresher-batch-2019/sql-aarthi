f5f7daaf6d1e9a9f7ddf4ad4131caa0454bbf6c5
Aarthi G <aarthiaruna12111999@gmail.com>
2019-12-26 10:49:46 +0530

    Update task.sql

--- task.sql
+++ task.sql
@@ -1,5 +1,5 @@
-create table tasks(task_no number,task_name varchar2(40),name varchar2(25),deadline date,completed_on date,status char(1),constraint task_no-pk primary key(task_no));
+create table tasks(task_no number,task_name varchar2(40) not null,name varchar2(25) not null,deadline date,completed_on date,status char(1),constraint task_no-pk primary key(task_no));
 
 insert into tasks(task_no,task_name,name,deadline,completed_on,status)values(1,'install oracle','aarthi',to_date('27-12-2019','dd-mm-yyyy'),to_date('26-12-2019',dd-mm-yyyy'),'yes');
 insert into tasks(task_no,task_name,name,deadline,completed_on,status)values(2,'install jre','rithya',to_date('14-12-2019','dd-mm-yyyy'),to_date('16-12-2019',dd-mm-yyyy'),'no');                                                                                                                       
-select * from tasks;
+select * from tasks where status='yes';
